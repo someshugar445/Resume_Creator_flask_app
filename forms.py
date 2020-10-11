d2e169284a8d8554e625032f4c0e630dd1c8e5d5
someshugar445 <Kamala@096>
2020-10-11 12:37:33 +0530

    added nav bar and styling to form

--- forms.py
+++ forms.py
@@ -4,10 +4,10 @@ from wtforms.validators import InputRequired
 
 
 class DownloadForm(Form):
-    name = StringField("Fullname", validators=[InputRequired('Please enter your name.')])
-    email = StringField("Email", validators=[InputRequired("Please enter your email address.")])
-    projects = StringField("Projects", validators=[InputRequired("Please enter a the projects.")])
-    education = StringField("Education:", validators=[InputRequired("Please enter education.")])
-    skills = StringField("Skills", validators=[InputRequired("Please enter the skills.")])
+    name = StringField("Fullname: ", validators=[InputRequired('Please enter your name.')])
+    email = StringField("Email: ", validators=[InputRequired("Please enter your email address.")])
+    projects = StringField("Projects: ", validators=[InputRequired("Please enter a the projects.")])
+    education = StringField("Education: ", validators=[InputRequired("Please enter education.")])
+    skills = StringField("Skills: ", validators=[InputRequired("Please enter the skills.")])
 
     submit = SubmitField("Download")
